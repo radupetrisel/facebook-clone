f7fe01acd70444bdc9212674c353c99479d954c1
Radu Petrisel <radupetrisel@gmail.com>
2024-03-13 21:13:02 +0200

    Setup project structure

---
+++ facebook-clone/App/facebook_cloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  facebook_cloneApp.swift
+//  facebook-clone
+//
+//  Created by Radu Petrisel on 13.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct facebook_cloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
